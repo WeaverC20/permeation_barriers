a4ce9ea4979aeef5a4e8392f46fae23d052ed057
J Dark <james.dark.95@gmail.com>
2022-07-11 12:53:20 +0000

    updated to FESTIM v0.10

--- solve_H_transport.py
+++ solve_H_transport.py
@@ -1,30 +1,13 @@
 from fenics import *
-import os, sys, inspect
-import sympy as sp
-import numpy as np
-from context import FESTIM
-
-from parameters import parameters
+from parameters import my_model
 
 
-def run_H_transport(parameters, log_level=20):
-    """Runs a hydrogen transport simulation 
-    Args:
-        parameters ([type]): [description]
-        S_0_lipb (float, optional): pre-exponential factor of solubility in
-            LiPb. Defaults to S_0_lipb.
-        E_S_lipb (float, optional): activation energy of solubility in LiPb.
-            Defaults to E_S_lipb.
-        log_level (int, optional): [description]. Defaults to 20.
-    Returns:
-        [type]: [description]
-    """
-    # create a simulation with these normal parameters
-    my_sim = FESTIM.Simulation(parameters, log_level=log_level)
-    my_sim.initialise()
+def run_H_transport():
+    """Runs a hydrogen transport simulation"""
 
-    return my_sim.run()
+    my_model.initialise()
+    my_model.run()
 
 
 if __name__ == "__main__":
-    run_H_transport(parameters)
+    run_H_transport()
