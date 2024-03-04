2a8b2061407af3f61c4c7ef6f341e814911032ca
J Dark <james.dark.95@gmail.com>
2024-03-04 20:06:38 +0100

    updated for modern matplotlib

--- analytical_studies/plot_profile.py
+++ analytical_studies/plot_profile.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
-from matplotlib import ticker, colors, cm
+from matplotlib import colors, cm
 import numpy as np
-from solve_3_layers import PRF_expression, res, a1, a2, a3, b1, b2, b3
+from solve_3_layers import res, a1, a2, a3, b1, b2, b3
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif', size=12)
 
 def compute_coeffs(e, L, S1, S2, D1, D2, pressure):
     coeffs = []
@@ -16,7 +14,7 @@ def compute_coeffs(e, L, S1, S2, D1, D2, pressure):
         coeff_sub = coeff_sub.subs("S2", S2)
         coeff_sub = coeff_sub.subs("D1", D1)
         coeff_sub = coeff_sub.subs("D2", D2)
-        coeff_sub = coeff_sub.subs("c_0", pressure**0.5*S1)
+        coeff_sub = coeff_sub.subs("c_0", pressure**0.5 * S1)
         coeff_sub = coeff_sub.subs("c_L", 0)
 
         coeffs.append(coeff_sub)
@@ -26,19 +24,22 @@ def compute_coeffs(e, L, S1, S2, D1, D2, pressure):
 def concentration(x, coeffs):
     a1, a2, a3, b1, b2, b3 = coeffs
     if 0 < x <= e:
-        return a1*x + b1
+        return a1 * x + b1
     elif e < x <= L + e:
-        return a2*x + b2
-    elif L + e < x <= L + 2*e:
-        return a3*x + b3
+        return a2 * x + b2
+    elif L + e < x <= L + 2 * e:
+        return a3 * x + b3
 
 
-param_vals = np.linspace(0.1, 1, num=4)
-sm = plt.cm.ScalarMappable(cmap=cm.Blues, norm=colors.Normalize(vmin=0, vmax=max(param_vals)))
+param_vals = np.linspace(0.1, 1, num=10)
+sm = plt.cm.ScalarMappable(
+    cmap=cm.Blues, norm=colors.Normalize(vmin=0, vmax=max(param_vals))
+)
 
 # varying D_coating only
+fig, axs = plt.subplots()
 for param in param_vals:
-    e = 0.1
+    e = 0.2
     L = 1
     S2 = 1
     D2 = 1
@@ -48,41 +49,23 @@ for param in param_vals:
 
     coeffs = compute_coeffs(e, L, S1, S2, D1, D2, pressure)
 
-    x = np.linspace(0, L + 2*e, num=200)
+    x = np.linspace(0, L + 2 * e, num=200)
     c = [concentration(x_val, coeffs) for x_val in x]
-    colour = cm.Blues(param/(param_vals.max()))
+    colour = cm.Blues(param / (param_vals.max()))
     plt.plot(x, c, color=colour)
 
-plt.colorbar(sm, label="$D_{\mathrm{coating}}$")
-plt.xlabel("x")
-plt.ylabel("c$_{\mathrm{m}}$")
-plt.show()
-
-# varying e only
-for param in param_vals:
-    e = param
-    L = 1
-    S2 = 1
-    D2 = 1
-    S1 = 1
-    D1 = 1
-    pressure = 1
-
-    coeffs = compute_coeffs(e, L, S1, S2, D1, D2, pressure)
-
-    x = np.linspace(0, L + 2*e, num=200)
-    c = [concentration(x_val, coeffs) for x_val in x]
-    colour = cm.Blues(param/(param_vals.max()))
-    plt.plot(x, c, color=colour)
-
-plt.colorbar(sm, label="$e$")
-plt.xlabel("x")
-plt.ylabel("c$_{\mathrm{m}}$")
-plt.show()
+plt.vlines(x=0.2, ymin=0, ymax=1, color="grey", linestyle="dashed", alpha=0.5)
+plt.vlines(x=1.2, ymin=0, ymax=1, color="grey", linestyle="dashed", alpha=0.5)
+fig.colorbar(sm, ax=axs, label=r"D$_{\mathrm{coating}}$")
+plt.xlim(0, 1.4)
+plt.ylim(0, 1)
+plt.xlabel(r"x")
+plt.ylabel(r"c$_{\mathrm{m}}$")
 
 # varying S_coating only
+fig, axs = plt.subplots()
 for param in param_vals:
-    e = 0.1
+    e = 0.2
     L = 1
     S2 = 1
     D2 = 1
@@ -92,12 +75,18 @@ for param in param_vals:
 
     coeffs = compute_coeffs(e, L, S1, S2, D1, D2, pressure)
 
-    x = np.linspace(0, L + 2*e, num=200)
+    x = np.linspace(0, L + 2 * e, num=200)
     c = [concentration(x_val, coeffs) for x_val in x]
-    colour = cm.Blues(param/(param_vals.max()))
+    colour = cm.Blues(param / (param_vals.max()))
     plt.plot(x, c, color=colour)
 
-plt.colorbar(sm, label="$S_{\mathrm{coating}}$")
-plt.xlabel("x")
-plt.ylabel("c$_{\mathrm{m}}$")
+plt.vlines(x=0.2, ymin=0, ymax=1, color="grey", linestyle="dashed", alpha=0.5)
+plt.vlines(x=1.2, ymin=0, ymax=1, color="grey", linestyle="dashed", alpha=0.5)
+fig.colorbar(sm, ax=axs, label=r"S$_{\mathrm{coating}}$")
+plt.xlim(0, 1.4)
+plt.ylim(0, 1)
+plt.xlabel(r"x")
+plt.ylabel(r"c$_{\mathrm{m}}$")
+
+
 plt.show()
