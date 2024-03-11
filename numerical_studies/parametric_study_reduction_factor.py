efaceb12add19bbd6793be17116c1275116c62d6
J Dark <james.dark.95@gmail.com>
2024-03-11 22:52:10 +0100

    simpler parametric study

--- numerical_studies/parametric_study_reduction_factor.py
+++ numerical_studies/parametric_study_reduction_factor.py
@@ -1,40 +1,20 @@
-from numerical_studies.festim_sim import my_model, eurofer
-import festim as F
+from festim_sim import festim_sim_permeation_barrier
 import numpy as np
-
-S_0_eur_original = 2.4088e23
-E_S_eurofer = 0.3026
 
 reduction_factors = np.geomspace(1e0, 1e08, num=50)
 
 folder = "Results/parametric_study/reduction_factor/"
 
-for export in my_model.exports.exports:
-    if isinstance(export, F.DerivedQuantities):
-        export.filename = folder + "standard_case/derived_quantities.csv"
-    elif isinstance(export, F.XDMFExport):
-        export.folder = folder + "standard_case/"
-        export.append = False
-        export.define_xdmf_file()
-
-my_model.initialise()
-my_model.run()
 
 for reduction_factor in reduction_factors:
-    reduced_S_0_eurofer = S_0_eur_original / reduction_factor
-    eurofer.S_0 = reduced_S_0_eurofer
-    eurofer.E_S = E_S_eurofer
 
-    results_folder = folder + "factor={:.1e}/".format(reduction_factor)
+    results_folder = folder + "prf={:.1e}/".format(reduction_factor)
 
-    for export in my_model.exports.exports:
-        if isinstance(export, F.DerivedQuantities):
-            export.filename = results_folder + "derived_quantities.csv"
-        elif isinstance(export, F.XDMFExport):
-            export.folder = results_folder
-            export.append = False
-            export.define_xdmf_file()
+    festim_sim_permeation_barrier(
+        PRF=reduction_factor, T=600, results_folder=results_folder
+    )
 
-    print("Reduction factor ={:.1e}".format(reduction_factor))
-    my_model.initialise()
-    my_model.run()
+# standard case
+results_folder = folder + "prf=1/"
+
+festim_sim_permeation_barrier(PRF=1, T=600, results_folder=results_folder)
