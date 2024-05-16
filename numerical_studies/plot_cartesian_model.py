516ab066315ef2e3052dbc48e048c480ee895670
J Dark <james.dark.95@gmail.com>
2024-05-16 15:46:24 +0200

    new barrier, with/without

--- numerical_studies/plot_cartesian_model.py
+++ numerical_studies/plot_cartesian_model.py
@@ -1,57 +1,55 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import shutil
+from matplotlib import cm, rcParams
 
+rcParams["text.usetex"] = True if shutil.which("latex") else False
 
 # with barrier
-datafile_with_barrier = "Results/cartesian_reproduction/profiles/with_barrier.csv"
+datafile_with_barrier = "Results/cartesian_reproduction/profiles/with_barrier.txt"
 data_with_barrier = np.genfromtxt(datafile_with_barrier, delimiter=",", names=True)
 solute_with_barrier = data_with_barrier["solute"]
 x_values = data_with_barrier["arc_length"]
 x_values *= 1e03
 
 # without barrier
-datafile_without_barrier = "Results/cartesian_reproduction/profiles/without_barrier.csv"
+datafile_without_barrier = "Results/cartesian_reproduction/profiles/without_barrier.txt"
 data_without_barrier = np.genfromtxt(
     datafile_without_barrier, delimiter=",", names=True
 )
 solute_without_barrier = data_without_barrier["solute"]
 
+
 # with modification
 datafile_with_modification = (
-    "Results/cartesian_reproduction/profiles/with_modification.csv"
+    "Results/cartesian_reproduction/profiles/with_modification.txt"
 )
 data_with_modification = np.genfromtxt(
     datafile_with_modification, delimiter=",", names=True
 )
 solute_with_modification = data_with_modification["solute"]
 
-
 plt.figure()
 
-plt.plot(x_values, solute_with_barrier, color="red", label="barrier")
+plt.plot(x_values, solute_with_barrier, color="red", label="with barrier")
 plt.plot(x_values, solute_without_barrier, color="black", label="without barrier")
-plt.plot(
-    x_values,
-    solute_with_modification,
-    color="grey",
-    linestyle="dashed",
-    label="modification",
-)
 
 plt.legend()
 plt.yscale("log")
-plt.ylabel(r"Solute concentration (H/m2)")
+plt.ylabel(r"Solute concentration (Hm$^{-3}$)")
 plt.xlabel(r"x (mm)")
-plt.ylim(1e06, 1e20)
+plt.ylim(1e09, 1e19)
 plt.xlim(0, 4)
 ax = plt.gca()
 ax.spines["top"].set_visible(False)
 ax.spines["right"].set_visible(False)
 plt.tight_layout()
+plt.savefig("Results/cartesian_reproduction/interface_condition_1.svg")
+
 
 plt.figure()
 
-plt.plot(x_values, solute_with_barrier, color="red", label="barrier")
+plt.plot(x_values, solute_with_barrier, color="red", label="with barrier")
 plt.plot(x_values, solute_without_barrier, color="black", label="without barrier")
 plt.plot(
     x_values,
@@ -63,9 +61,26 @@ plt.plot(
 
 plt.legend()
 plt.yscale("log")
-plt.ylabel(r"Solute concentration (H/m2)")
+plt.ylabel(r"Solute concentration (Hm$^{-3}$)")
 plt.xlabel(r"x (mm)")
-plt.ylim(1e010, 1e20)
+plt.ylim(1e09, 1e19)
+plt.xlim(0, 4)
+ax = plt.gca()
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+plt.tight_layout()
+plt.savefig("Results/cartesian_reproduction/interface_condition_2.svg")
+
+plt.figure()
+
+plt.plot(x_values, solute_with_barrier, color="red", label="barrier")
+plt.plot(x_values, solute_without_barrier, color="black", label="without barrier")
+
+plt.legend()
+plt.yscale("log")
+plt.ylabel(r"Solute concentration (Hm$^{-3}$)")
+plt.xlabel(r"x (mm)")
+plt.ylim(1e011, 1e19)
 e = 5e-03
 xmin = 2 - 2 * e
 xmax = 2 + 2 * e
@@ -74,6 +89,35 @@ ax = plt.gca()
 ax.spines["top"].set_visible(False)
 ax.spines["right"].set_visible(False)
 plt.tight_layout()
+plt.savefig("Results/cartesian_reproduction/interface_condition_zoom_1.svg")
 
 
-plt.show()
+plt.figure()
+
+plt.plot(x_values, solute_with_barrier, color="red", label="with barrier")
+plt.plot(x_values, solute_without_barrier, color="black", label="without barrier")
+plt.plot(
+    x_values,
+    solute_with_modification,
+    color="grey",
+    linestyle="dashed",
+    label="with modification",
+)
+
+plt.legend()
+plt.yscale("log")
+plt.ylabel(r"Solute concentration (Hm$^{-3}$)")
+plt.xlabel(r"x (mm)")
+plt.ylim(1e011, 1e19)
+e = 5e-03
+xmin = 2 - 2 * e
+xmax = 2 + 2 * e
+plt.xlim(xmin, xmax)
+ax = plt.gca()
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+plt.tight_layout()
+plt.savefig("Results/cartesian_reproduction/interface_condition_zoom_2.svg")
+
+
+# plt.show()
