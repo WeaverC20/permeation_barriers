2e83cb139343b001f7e1a6a71efcdc1be6a0a621
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2025-03-11 14:08:52 -0400

    plugged in vals for inconel and 316l ss and added on analytical W

--- analytical_studies/compute_W_2_layers_open.py
+++ analytical_studies/compute_W_2_layers_open.py
@@ -6,11 +6,14 @@ c1, cm1, cm2, c2 = sp.symbols('c1 cm1 cm2 c2')
 D1, D2 = sp.symbols('D1 D2')
 P = sp.Symbol('P')
 x = sp.Symbol('x')
+xL, xint, x0 = sp.symbols('xL xint x0')
+e1, e2 = sp.symbols('e1 e2')
 
 # Flux balance
 expr1 = Kd1*P - Kr1*c1**2
 expr2 = -D1*(c1 - cm2/Ks2*Ks1)/x
 expr3 = -D2*(cm2/Ks1*Ks2 - c2)/x
+exprD = -D2*((D1*(xL-xint)*c1 + D2*(xint-x0)*c2)/(D2*(xint-x0) + D1*(xL-xint)*Ks1/Ks2) - c2)
 expr4 = Kr2*c2**2
 
 # Dimensionless surface limited flux
@@ -20,10 +23,14 @@ JSL = Kd1*P * (1 + (Kr1-Kr2)/(Kr1+Kr2))
 eq1 = sp.Eq(expr1/JSL, expr2/JSL)
 eq2 = sp.Eq(expr2/JSL, expr3/JSL)
 eq3 = sp.Eq(expr3/JSL, expr4/JSL)
+
+eq1 = sp.Eq(expr1/JSL, exprD/JSL)
+eq2 = sp.Eq(exprD/JSL, expr4/JSL)
 # eq4 = sp.Eq(Ks1, (Kd1/Kr1)**(1/2))
 
 # Combine them
-all_eqs = sp.And(eq1, eq2, eq3)
+# all_eqs = sp.And(eq1, eq2, eq3)
+all_eqs = sp.And(eq1, eq2)
 
 # Factoring Eqs
 factored_eqs = sp.factor_terms(all_eqs)
@@ -33,6 +40,58 @@ print("\n\n\n")
 
 # extracting factors of important vars
 important_vars = [c1, c2]
-collected_eqs = [sp.collect(eq.lhs - eq.rhs, var) for eq in [eq1, eq2, eq3] for var in important_vars]
+collected_eqs = [sp.collect(eq.lhs - eq.rhs, var) for eq in [eq1, eq2] for var in important_vars]
 for eq in collected_eqs:
     sp.pprint(eq)
+
+
+print('\n\n\n')
+
+u, v = sp.symbols('x, y')
+exprD2 = D1*(xL-xint)*(Kd1*Kr1*P)**(-1/2)*v + D2*(xint-x0)*(Kd1*Kr2*P)**(-1/2)*u
+factored_expr = exprD2.collect([u, v])
+print(factored_expr)
+
+expr5 = (Ks2*(Kd1/Kr1*Kr2/Kd2*P*(1+Kd2/(Kd1+Kd2)))**(1/2))
+expr5 = sp.simplify(expr5)
+print("\n\n\n\n")
+sp.pprint(expr5)
+
+# import sympy as sp
+
+# # Define symbols
+# Kd1, Kd2, Kr1, Kr2, Ks1, Ks2 = sp.symbols('Kd1 Kd2 Kr1 Kr2 Ks1 Ks2')
+# c1, cm1, cm2, c2 = sp.symbols('c1 cm1 cm2 c2')
+# D1, D2 = sp.symbols('D1 D2')
+# P, x = sp.symbols('P x')
+
+# # Flux balance
+# expr1 = Kd1*P - Kr1*c1**2
+# expr2 = -D1*(c1 - cm2 / (Ks2 * Ks1)) / x
+# expr3 = -D2*(cm2 / (Ks1 * Ks2) - c2) / x
+# expr4 = Kr2*c2**2
+
+# # Dimensionless surface-limited flux
+# JSL = Kd1*P * (1 + (Kr1 - Kr2) / (Kr1 + Kr2))
+
+# # Flux continuity equations
+# eq1 = sp.Eq(expr1 / JSL, expr2 / JSL)
+# eq2 = sp.Eq(expr2 / JSL, expr3 / JSL)
+# eq3 = sp.Eq(expr3 / JSL, expr4 / JSL)
+
+# # Convert equations to expressions for simplification
+# eqs_exprs = [eq.lhs - eq.rhs for eq in [eq1, eq2, eq3]]
+
+# # Factors to extract
+# factor1 = c1 / (Ks1 * sp.sqrt(P))
+# factor2 = c2 / (Ks2 * sp.sqrt(P))
+
+# # Collecting expressions with respect to the factors
+# factored_exprs = [sp.collect(expr, [factor1, factor2]) for expr in eqs_exprs]
+
+# # Convert back to equations
+# factored_eqs = [sp.Eq(expr, 0) for expr in factored_exprs]
+
+# # Print results
+# for eq in factored_eqs:
+#     sp.pprint(eq)
--- analytical_studies/solve_2_layers_open.py
+++ analytical_studies/solve_2_layers_open.py
@@ -4,7 +4,8 @@ from sympy import *
 D1, D2 = symbols('D1, D2')
 x_int = Symbol("x_int")
 S1, S2 = symbols('S1, S2')
-Kd, Kr = symbols('Kd, Kr')
+Kd1, Kr1 = symbols('Kd1, Kr1')
+Kd2, Kr2 = symbols('Kd2, Kr2')
 u_0, u_L = symbols('c_0, c_L')
 a1, a2, b1, b2 = symbols('a1, a2, b1, b2')
 x_L = Symbol('L')
@@ -19,8 +20,9 @@ u2 = a2*x + b2
 list_of_equations = [
     u1.subs(x, x_int)/S1 - u2.subs(x, x_int)/S2, # Continuity at x_int
     D1*diff(u1, x).subs(x, x_int) - D2*diff(u2, x).subs(x, x_int), # Flux continuity at x_int
-    diff(u1, x).subs(x, x_0) + Kd*P - Kr*u1.subs(x, x_0), # Neumann boundary condition at x_0
-    u2.subs(x, x_L), #concentration = 0 at x_L
+    diff(u1, x).subs(x, x_0) + Kd1*P - Kr1*u1.subs(x, x_0), # Neumann boundary condition at x_0
+    # u2.subs(x, x_L), #concentration = 0 at x_L
+    diff(u1, x).subs(x, x_L) - Kr2*u2.subs(x, x_L), # Neumann boundary condition at x_L
 
     # Kd*P-Kr1*u1.subs(x, x_0)**2 + D1*(u1.subs(x, x_int) - u1.subs(x, x_0))/(x_int-x_0), # Flux Continuity at x_0
     # u1.subs(x, x_int)/S1 - u2.subs(x, x_int)/S2,  # Continuity at x_int
@@ -47,29 +49,19 @@ import matplotlib.pyplot as plt
 
 # Substitute numerical values for all parameters into the solutions
 params = {
-    D1: 1, # tungsten
-    D2: 10, # eurofer
-    S1: 1,
-    S2: 5,
-    x_int: 0.1,
-    x_L: 1,
-    P: 100,
-    Kd: 1,
-    Kr: 1,
+    D1: 6e-11, # inconel
+    D2: 1.54e-11, # 316l steel
+    S1: sqrt(3e13/(2.68e-30)),
+    S2: sqrt(8e11/(2.4e-33)),
+    x_0: 0,
+    x_int: 0.001,
+    x_L: 0.01,
+    P: 100000,
+    Kd1: 3e13, # or 6e16
+    Kr1: 2.68e-30,
+    Kd2: 8e11, # or 6e16
+    Kr2: 2.4e-33, # or 2e-28
 }
-
-# # Substitute numerical values for all parameters into the solutions
-# params = {
-#     D1: 4e-10, # tungsten
-#     D2: 1e-8, # eurofer
-#     S1: 1.13e20,
-#     S2: 1.77e20,
-#     x_int: 0.01,
-#     x_L: 0.1,
-#     P: 100000,
-#     Kd: 1.5e-24, # no dissociation coeff
-#     Kr: 1.5e-23, # tungsten
-# }
 
 # sub soolutions into u1 u2
 u1_sol = u1.subs(res).subs(params)
@@ -84,6 +76,15 @@ x2_vals = np.linspace(params[x_int], params[x_L], 100)
 u1_vals = u1_num(x1_vals)
 u2_vals = u2_num(x2_vals)
 
+print("\nC1 =", u1_vals[0])
+print("C2 =", u2_vals[-1], "\n")
+
+W = params[Kd1]*params[P]*(params[D2]*(params[x_int]-params[x_0])/params[S1] + params[D1]*(params[x_L]-params[x_int])/params[S2])/(-params[D2]*params[D1]*params[P]**1/2)
+
+print("W =", W)
+print("diffusion limited")
+
+
 plt.figure(figsize=(8, 5))
 plt.plot(x1_vals, u1_vals, label="u1(x)", color="blue")
 plt.plot(x2_vals, u2_vals, label="u2(x)", color="red")
@@ -94,38 +95,34 @@ plt.legend()
 plt.grid()
 plt.show()
 
+# # normalized
 
+# c_L = Symbol('c_L')
+# c_0 = 1
+# alpha, beta, gamma = symbols('alpha, beta, gamma') # gamma = x_int
+# a1, a2, b1, b2 = symbols('a1, a2, b1, b2')
+# x_L = 1
+# x_0 = 0
+# x = Symbol("x")
 
+# u1 = a1*x + b1
+# u2 = a2*x + b2
 
+# list_of_equations = [
+#     u1.subs(x, gamma)*beta - u2.subs(x, x_int), # Continuity at x_int
+#     diff(u1, x).subs(x, gamma) - alpha*diff(u2, x).subs(x, gamma), # Flux continuity at x_int
+#     diff(u1, x).subs(x, x_0) - Kd*P + Kr*u1.subs(x, x_0), # Neumann boundary condition at x_0
+#     u2.subs(x, x_L), #concentration = 0 at x_L
+# ]
 
-# normalized
+# list_of_equations = [
+#     u1.subs(x, x_int)/S1 - u2.subs(x, x_int)/S2, # Continuity at x_int
+#     D1*diff(u1, x).subs(x, x_int) - D2*diff(u2, x).subs(x, x_int), # Flux continuity at x_int
+#     diff(u1, x).subs(x, x_0) - Kd*P + Kr*u1.subs(x, x_0), # Neumann boundary condition at x_0
+#     u2.subs(x, x_L), #concentration = 0 at x_L
+# ]
 
-c_L = Symbol('c_L')
-c_0 = 1
-alpha, beta, gamma = symbols('alpha, beta, gamma') # gamma = x_int
-a1, a2, b1, b2 = symbols('a1, a2, b1, b2')
-x_L = 1
-x_0 = 0
-x = Symbol("x")
-
-u1 = a1*x + b1
-u2 = a2*x + b2
-
-list_of_equations = [
-    u1.subs(x, gamma)*beta - u2.subs(x, x_int), # Continuity at x_int
-    diff(u1, x).subs(x, gamma) - alpha*diff(u2, x).subs(x, gamma), # Flux continuity at x_int
-    diff(u1, x).subs(x, x_0) - Kd*P + Kr*u1.subs(x, x_0), # Neumann boundary condition at x_0
-    u2.subs(x, x_L), #concentration = 0 at x_L
-]
-
-list_of_equations = [
-    u1.subs(x, x_int)/S1 - u2.subs(x, x_int)/S2, # Continuity at x_int
-    D1*diff(u1, x).subs(x, x_int) - D2*diff(u2, x).subs(x, x_int), # Flux continuity at x_int
-    diff(u1, x).subs(x, x_0) - Kd*P + Kr*u1.subs(x, x_0), # Neumann boundary condition at x_0
-    u2.subs(x, x_L), #concentration = 0 at x_L
-]
-
-res = solve(list_of_equations, a1, a2, b1, b2)
+# res = solve(list_of_equations, a1, a2, b1, b2)
 
 # print(simplify(res[a1]).subs(f1, f*alpha**0.5).subs(f2, f*alpha**-0.5))
 # print(simplify(res[a2]).subs(f1, f*alpha**0.5).subs(f2, f*alpha**-0.5))
